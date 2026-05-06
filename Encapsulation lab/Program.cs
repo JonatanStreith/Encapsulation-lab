e326e0e98d25211e38b9aa7dc9245b42113175d7
Jonatan Streith <jonatan@streith.se>
2026-05-06 15:17:26 +0200

    Add project files.

---
+++ Encapsulation lab/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Encapsulation_lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
