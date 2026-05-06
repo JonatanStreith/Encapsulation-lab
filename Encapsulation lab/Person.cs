879811169cf6a707acf2ac198a7aa4381543af8c
Jonatan Streith <jonatan@streith.se>
2026-05-06 15:28:52 +0200

    Created a Person class with a primary constructor.

---
+++ Encapsulation lab/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encapsulation_lab
+{
+    internal class Person (string firstName, string lastName, int age)
+    {
+        private string FirstName = firstName;
+        private string LastName = lastName;
+        private int Age = age;
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} is {Age} years old.";
+        }
+    }
+}
