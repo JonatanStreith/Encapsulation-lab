3756564e5c8b065b6ae2d000116e4eb87659fbd9
Jonatan Streith <jonatan@streith.se>
2026-05-06 16:11:36 +0200

    Changed main method in accordance with exercise code. Accounted for error in writing.

--- Encapsulation lab/Program.cs
+++ Encapsulation lab/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Encapsulation_lab
+﻿using System.Globalization;
+
+namespace Encapsulation_lab
 {
     internal class Program
     {
@@ -6,17 +8,18 @@
         {
             var lines = 5;
             var persons = new List<Person>();
-            for (int i = 0; i < lines; i++)
+                for (int i = 0; i < lines; i++)
             {
                 var cmdArgs = Console.ReadLine().Split();
-                var person = new Person(cmdArgs[0], cmdArgs[1], int.Parse(cmdArgs[2]));
+                var person = new Person(cmdArgs[0],
+                cmdArgs[1],
+                int.Parse(cmdArgs[2]),
+                decimal.Parse(cmdArgs[3]));
                 persons.Add(person);
             }
-            persons.OrderBy(p => p.FirstName)
-            .ThenBy(p => p.Age)
-            .ToList()
-            .ForEach(p => Console.WriteLine(p.ToString()));
-
+            var bonus = decimal.Parse(Console.ReadLine());
+            persons.ForEach(p => p.IncreaseSalary(bonus));
+            persons.ForEach(p => Console.WriteLine(p.ToString()));
         }
     }
 }
