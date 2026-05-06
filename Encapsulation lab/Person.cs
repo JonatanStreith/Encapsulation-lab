5f52d764bf8ca8e9cde582d4539a81b05181f21a
Jonatan Streith <jonatan@streith.se>
2026-05-06 16:36:15 +0200

    Throwing exceptions when inputs are faulty.

--- Encapsulation lab/Person.cs
+++ Encapsulation lab/Person.cs
@@ -1,15 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.Metrics;
 using System.Text;
+using System.Xml.Linq;
 
 namespace Encapsulation_lab
 {
     internal class Person(string firstName, string lastName, int age, decimal salary)
     {
-        public string FirstName { get; private set; } = firstName;
-        public string LastName { get; private set; } = lastName;
-        public int Age { get; private set; } = age;
-        public decimal Salary { get; private set; } = salary;
+        public string FirstName
+        {
+            get;
+            private set { if (value.Length < 3) throw new ArgumentException("First name cannot contain fewer than 3 symbols!"); }
+        } = firstName;
+        public string LastName
+        {
+            get;
+            private set { if (value.Length < 3) throw new ArgumentException("Last name cannot contain fewer than 3 symbols!"); }
+        } = lastName;
+        public int Age
+        {
+            get;
+            private set { if (value < 0) throw new ArgumentException("Age cannot be zero or a negative integer!"); }
+        } = age;
+        public decimal Salary
+        {
+            get;
+            private set { if (value < 460) throw new ArgumentException("Salary cannot be less than 460 dollar!"); }
+        } = salary;
 
         public override string ToString()
         {
