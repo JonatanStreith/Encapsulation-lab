7152bb5ca48c461d2b98fdbff3586c6ce8590b46
Jonatan Streith <jonatan@streith.se>
2026-05-07 08:29:15 +0200

    Attempting to add ArgumentException to handle illegal inputs. However, this is not working well and we will need unit testing to examine.

--- Encapsulation lab/Person.cs
+++ Encapsulation lab/Person.cs
@@ -12,22 +12,26 @@ namespace Encapsulation_lab
         public string FirstName
         {
             get;
-            private set { if (value.Length < 3) throw new ArgumentException("First name cannot contain fewer than 3 symbols!"); }
+            private set { if (value.Length < 3) throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+            }
         } = firstName;
         public string LastName
         {
             get;
-            private set { if (value.Length < 3) throw new ArgumentException("Last name cannot contain fewer than 3 symbols!"); }
+            private set { if (value.Length < 3) throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+            }
         } = lastName;
         public int Age
         {
             get;
-            private set { if (value < 0) throw new ArgumentException("Age cannot be zero or a negative integer!"); }
+            private set { if (value < 0) throw new ArgumentException("Age cannot be zero or a negative integer!");
+            }
         } = age;
         public decimal Salary
         {
             get;
-            private set { if (value < 460) throw new ArgumentException("Salary cannot be less than 460 dollar!"); }
+            private set { if (value < 460) throw new ArgumentException("Salary cannot be less than 460 dollar!");
+            }
         } = salary;
 
         public override string ToString()
--- Encapsulation lab/Program.cs
+++ Encapsulation lab/Program.cs
@@ -8,15 +8,20 @@ namespace Encapsulation_lab
         {
             var lines = 5;
             var persons = new List<Person>();
-                for (int i = 0; i < lines; i++)
+            try
             {
-                var cmdArgs = Console.ReadLine().Split();
-                var person = new Person(cmdArgs[0],
-                cmdArgs[1],
-                int.Parse(cmdArgs[2]),
-                decimal.Parse(cmdArgs[3].Replace(".", ",")));
-                persons.Add(person);
+                for (int i = 0; i < lines; i++)
+                {
+                    var cmdArgs = Console.ReadLine().Split();
+                    var person = new Person(cmdArgs[0],
+                    cmdArgs[1],
+                    int.Parse(cmdArgs[2]),
+                    decimal.Parse(cmdArgs[3].Replace(".", ",")));
+                    persons.Add(person);
+                }
             }
+            catch (Exception e) { Console.WriteLine(e.Message); }
+
             var bonus = decimal.Parse(Console.ReadLine().Replace(".", ","));
             persons.ForEach(p => p.IncreaseSalary(bonus));
             persons.ForEach(p => Console.WriteLine(p.ToString()));
