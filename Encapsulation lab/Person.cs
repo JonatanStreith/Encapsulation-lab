6f212f56cec36abbb5b3d8c362d1da2dbb11fdbb
Jonatan Streith <jonatan@streith.se>
2026-05-07 09:20:32 +0200

    Changed Person constructor into a regular constructor.

--- Encapsulation lab/Person.cs
+++ Encapsulation lab/Person.cs
@@ -7,32 +7,40 @@ using System.Xml.Linq;
 
 namespace Encapsulation_lab
 {
-    public class Person(string firstName, string lastName, int age, decimal salary)
+    public class Person
     {
         public string FirstName
         {
             get;
-            private set { if (value.Length < 3) throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+            private set { if (value.Length < 3) throw new ArgumentException("First name cannot contain fewer than 3 symbols!"); 
             }
-        } = firstName;
+        } 
         public string LastName
         {
             get;
             private set { if (value.Length < 3) throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
             }
-        } = lastName;
+        } 
         public int Age
         {
             get;
             private set { if (value < 0) throw new ArgumentException("Age cannot be zero or a negative integer!");
             }
-        } = age;
+        } 
         public decimal Salary
         {
             get;
             private set { if (value < 460) throw new ArgumentException("Salary cannot be less than 460 dollar!");
             }
-        } = salary;
+        } 
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
 
         public override string ToString()
         {
